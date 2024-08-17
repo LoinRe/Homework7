f84c6a3c8c1c507ac138207b808420913d110d92
LoinRe <winard2@mail.ru>
2024-08-17 20:15:50 +0300

    Исправлено ДЗ

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
         int depositMonth = 15000;
         int totalMoney = 0;
         int numMonth = 0;
-        while (totalMoney < 2459000) {
-            totalMoney += totalMoney / 100;
+        int wishSum = 2459000;
+        double percent = 1D / 100;
+        while (totalMoney < wishSum) {
             totalMoney = totalMoney + depositMonth;
+            totalMoney = (int) (totalMoney * (1 + percent));
             numMonth++;
         }
         System.out.println("Месяц " + numMonth + ", сумма накоплений равна " + totalMoney + " рублей");
@@ -16,7 +18,7 @@ public class Main {
         System.out.println("Задание 2");
 // Задание 2
         int num = 0;
-        while (num <= 9) {
+        while (num < 10) {
             num++;
             System.out.print(num + " ");
         }
@@ -33,12 +35,12 @@ public class Main {
         int birth = 17;
         int death = 8;
         int naturalIncrease = birth - death; // на тысячу
-        int increaseInYear = naturalIncrease * 12_000_000 / 1000;
+        int increaseInYear = naturalIncrease * population / 1000;
         int year = 2024;
         while (year < 2034) {
             population += increaseInYear;
-            year++;
             System.out.println("Год " + year + ", численность населения составляет " + population);
+            year++;
         }
 
 
@@ -46,9 +48,11 @@ public class Main {
 // Задание 4
         int deposit = 15000;
         int month = 0;
-        while (deposit < 12000000) {
+        percent = 7D / 100;
+        wishSum = 12000000;
+        while (deposit < wishSum) {
             month++;
-            deposit += deposit * 7 / 100;
+            deposit = (int) (deposit * (1 + percent));
             System.out.println("Месяц " + month + " Итого " + deposit);
         }
         System.out.println(month + " месяцев нужно копить");
@@ -56,27 +60,28 @@ public class Main {
 
         System.out.println("Задание 5");
 // Задание 5
-        int deposit1 = 15000;
-        int month1 = 0; // подсчет месяцев
-        while (deposit1 < 12000000) {
-            month1++;
-            deposit1 += deposit1 * 7 / 100;
-            if (month1 % 6 == 0) {
-                System.out.println("Месяц " + month1 + " Итого " + deposit1);
+        deposit = 15000;
+        month = 0; // подсчет месяцев
+        percent = 7D / 100;
+        while (deposit < wishSum) {
+            month++;
+            deposit = (int) (deposit * (1 + percent));
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + " Итого " + deposit);
             }
         }
-        System.out.println(month1 + " месяцев нужно копить");
+        System.out.println(month + " месяцев нужно копить");
 
 
         System.out.println("Задание 6");
 // Задание 6
-        int deposit2 = 15000;
+        deposit = 15000;
         int timeToSaveMoney = 0; // 108 месяцев = 9 лет
         while (timeToSaveMoney < 108) {
             timeToSaveMoney++;
-            deposit2 += deposit2 * 7 / 100;
+            deposit = (int) (deposit * (1 + percent));
             if (timeToSaveMoney % 6 == 0) {
-                System.out.println("Месяц " + timeToSaveMoney + " Итого " + deposit2);
+                System.out.println("Месяц " + timeToSaveMoney + " Итого " + deposit);
             }
         }
 
@@ -88,26 +93,14 @@ public class Main {
             System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет");
         }
 
-// Еще одно решение:
-//         int monthDay = 0;
-//         while (monthDay < 31) {
-//             monthDay++;
-//             if (monthDay % 7 == 0) {
-//                 System.out.println("Сегодня пятница, " + numFriday + "-е число. Необходимо подготовить отчет");
-//                 numFriday += 7;
-//             }
-//         }
-
-        // Кажется, с ним что то не то, но оно работает))
-
 
         System.out.println("Задание 8");
 // Задание 8
-        int arrive = 79;
-        int yearNow = 2000;
+        int period = 79;
+        int yearNow = 2024;
         int yearFuture = yearNow + 100;
         int yearPast = yearNow - 200;
-        for (int i = 0; i < yearFuture; i += arrive) {    // период от нулевого года до 2100
+        for (int i = 0; i < yearFuture; i += period) {    // период от нулевого года до 2100
             if (i >= yearPast && i < yearNow) {           // для последних 200 лет
                 System.out.println("Комета прилетала: " + i);
             }
@@ -117,3 +110,6 @@ public class Main {
         }
     }
 }
+
+
+// У меня 79 не было магическим числом. Поменяла название с arrive на period, хотя имя вроде и так понятным было.
